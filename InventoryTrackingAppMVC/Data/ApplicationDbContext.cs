aafa92f9000c59b9d6fe8300460853a1bd9b6209
Harish Nagallapati <harishnagallapati21@gmail.com>
2022-01-19 21:16:07 -0500

    Initial commit

---
+++ InventoryTrackingAppMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using InventoryTrackingAppMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryTrackingAppMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ InventoryTrackingAppMVC/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/InventoryTrackingAppMVC.Data.ApplicationDbContext.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220119205948_AddProductToDatabase')
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Brand] nvarchar(max) NOT NULL,
+        [Quantity] int NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220119205948_AddProductToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220119205948_AddProductToDatabase', N'6.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
