b963217003ab4df15e268941726eba6ad5f53a82
Harish Nagallapati <harishnagallapati21@gmail.com>
2022-01-19 21:34:58 -0500

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,131 @@
+﻿using CsvHelper;
+using InventoryTrackingAppMVC.Data;
+using InventoryTrackingAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace InventoryTrackingAppMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _db.Products;
+            return View(objProductList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+            
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var productFromDb = _db.Products.Find(id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var productFromDb = _db.Products.Find(id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Products.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Products.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+        public FileContentResult ExportToCSV()
+        {
+            var product = _db.Products.ToList();
+            StringWriter sw = new StringWriter();
+            sw.WriteLine("\"Id\",\"Name\",\"Brand\",\"Quantity\"");
+
+            foreach (var prod in product)
+            {
+                sw.WriteLine(string.Format("\"{0}\",\"{1}\",\"{2}\",\"{3}\"",
+                     prod.Id,
+                                           prod.Name,
+                                           prod.Brand,
+                                           prod.Quantity));
+            }
+
+            var fileName = "ProductList" + DateTime.Now.ToString() + ".csv";
+            return File(new System.Text.UTF8Encoding().GetBytes(sw.ToString()), "text/csv", fileName);
+        }
+    }
+}
